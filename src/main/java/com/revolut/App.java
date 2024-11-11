7ab11b0701426bf188ea5f5f214d7d4ee6661e20
Maksym Bielushkin <maxb@getblend.com>
2024-11-11 19:13:36 +0100

    added

--- src/main/java/com/revolut/App.java
+++ src/main/java/com/revolut/App.java
@@ -5,13 +5,12 @@ import com.google.inject.Injector;
 import com.revolut.account.controller.AccountController;
 import com.revolut.config.MainModule;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    Injector injector = Guice.createInjector(new MainModule());
+public class App {
 
-    AccountController accountController = injector.getInstance(AccountController.class);
-    accountController.configureRoutes();
+    public static void main(String[] args) {
+        Injector injector = Guice.createInjector(new MainModule());
+
+        AccountController accountController = injector.getInstance(AccountController.class);
+        accountController.configureRoutes();
     }
 }
